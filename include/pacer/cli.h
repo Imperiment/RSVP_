be0746ea6bfc9ea2d6dbb28ad7bce1e39759e39e
MacneDav <imperiment@gmail.com>
2026-07-21 11:47:59 +0200

    cli.h base structure implementation

---
+++ include/pacer/cli.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+
+constexpr int DEFAULT_WPM = 300;
+
+struct CliOptions {
+    std::string filePath;
+    int wordsPerMinute = DEFAULT_WPM;
+    bool startPaused = false;
+};
+
+class Cli {
+  public:
+    static CliOptions parse(int arc, char **argv);
+};
