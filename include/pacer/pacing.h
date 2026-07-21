22734aab846a0ae9cb1dfbeabb70e31e97aefe89
MacneDav <imperiment@gmail.com>
2026-07-21 11:48:14 +0200

    pacing structure implementation

---
+++ include/pacer/pacing.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "tokenizer.h"
+#include <chrono>
+
+class Pacing {
+  public:
+    explicit Pacing(int wordsPerMinute);
+
+    [[nodiscard]] std::chrono::milliseconds delayFor(const Token &token) const;
+
+    [[nodiscard]] std::size_t wordsInDuration(std::chrono::milliseconds duration) const;
+
+    void setWordsPerMinute(int wpm);
+    [[nodiscard]] int wordsPerMinute() const;
+
+  private:
+    int m_wpm;
+};
